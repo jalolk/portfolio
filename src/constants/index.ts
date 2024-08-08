fda6256874842fc48be426c14502972f6370eb7e
jalolk <jalol.kham.24@gmail.com>
2024-08-08 22:45:13 +0100

    core(base): created a base portfolio page

---
+++ src/constants/index.ts
@@ -0,0 +1,18 @@
+import { config } from "../data/config"
+
+export const navigationLinks = {
+    "Blogs": "/blogs",
+    "Projects": "/projects",
+}
+
+export const BLOGS_SECTION_TITLES = {
+    title: "Latest from the Blog",
+    subtitle: "Explore my thoughts, insights, and latest updates in the world of technology, design, and beyond.",
+}
+
+export const PROJECTS_SECTION_TITLES = {
+    title: "Projects",
+    subtitle: "Here are some of the projects I've worked on.",
+}
+
+export const FOOTER_NOTE = `© 2024 ${config.name}. Powered by caffeine and a lot of late nights.`
