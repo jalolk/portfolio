06cba44f96d3c52f5d3ede54d559f78e75ac1c60
Jalol Khamroev <jalol.khamroev@toptal.com>
2024-10-25 02:02:10 +0500

    docs: added more projects

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,4 +1,10 @@
-import { Toptal, Fujitsu, Github, Linkedin, Stackoverflow } from "simple-icons-astro";
+import {
+  Toptal,
+  Fujitsu,
+  Github,
+  Linkedin,
+  Stackoverflow,
+} from "simple-icons-astro";
 import { University, GraduationCap } from "lucide-astro";
 import {
   React,
@@ -48,22 +54,25 @@ export const config = {
     },
   ],
   profile: [
-    {title: "Education", history: [
-      {
-        title: "M.S. Computer Science",
-        establishment: "University of Lodz",
-        startYear: "2019",
-        endYear: "2022",
-        Logo: University,
-      },
-      {
-        title: "B.S. Computer Science",
-        establishment: "University of Lodz",
-        startYear: "2023",
-        endYear: "2025",
-        Logo: GraduationCap,
-      },
-    ] },
+    {
+      title: "Education",
+      history: [
+        {
+          title: "M.S. Computer Science",
+          establishment: "University of Lodz",
+          startYear: "2019",
+          endYear: "2022",
+          Logo: University,
+        },
+        {
+          title: "B.S. Computer Science",
+          establishment: "University of Lodz",
+          startYear: "2023",
+          endYear: "2025",
+          Logo: GraduationCap,
+        },
+      ],
+    },
     {
       title: "Experience",
       history: [
@@ -82,12 +91,33 @@ export const config = {
           Logo: Fujitsu,
         },
       ],
-    }
+    },
   ],
   projects: [
     {
+      title:
+        "Play2do: Comprehensive QA & Testing Automation for Task Manager App",
+      description:
+        "Play2do is a task management app showcasing best practices in QA automation and testing. Built with NestJS, React, Vite, and MongoDB, it demonstrates unit, integration, and end-to-end testing using Jest, Playwright, and Bruno for API testing. Ideal for QA engineers and developers looking to implement comprehensive testing strategies in modern web apps.",
+      image: "/images/project/play2do.webp",
+      name: "Play2Do",
+      githubLink: "https://github.com/jalolk/play2do",
+      tools: ["NestJS", "Vite", "MongoDB", "Jest", "Playwright", "Bruno"],
+    },
+    {
+      title: "ClimQA: Frontend Testing Template with CI/CD & Automation",
+      description:
+        "A comprehensive QA-focused template demonstrating industry best practices for frontend testing architecture. Uses a weather dashboard as a practical example to showcase various testing strategies and CI/CD implementation.",
+      image: "/images/project/climqa.webp",
+      name: "ClimQA",
+      githubLink: "https://github.com/jalolk/climqa",
+      demoLink: "https://climqa-cfcysydrr-jalolks-projects.vercel.app/",
+      tools: ["Vite", "Playwright", "GitHub Actions"],
+    },
+    {
       title: "Astro-Powered SEO-Optimized Portfolio",
-      description: "Built a lightning-fast portfolio website using Astro, TypeScript, and TailwindCSS, showcasing the power of static site generation with modern web technologies. The site features blazing fast load times, an SEO-optimized structure, responsive design, an integrated Markdown-supported blog, and a dynamic project showcase, all optimized for performance and user experience. This project demonstrates proficiency in modern web development, emphasizing speed, SEO, and maintainability, serving as both a personal showcase and a high-performance template for developers.",
+      description:
+        "Built a lightning-fast portfolio website using Astro, TypeScript, and TailwindCSS, showcasing the power of static site generation with modern web technologies. The site features blazing fast load times, an SEO-optimized structure, responsive design, an integrated Markdown-supported blog, and a dynamic project showcase, all optimized for performance and user experience. This project demonstrates proficiency in modern web development, emphasizing speed, SEO, and maintainability, serving as both a personal showcase and a high-performance template for developers.",
       image: "/images/project/portfolio.webp",
       name: "Portfolio",
       githubLink: "https://github.com/jalolk/portfolio",
@@ -114,5 +144,5 @@ export const config = {
     Githubactions,
     Graphql,
     Postman,
-  ]
+  ],
 };
